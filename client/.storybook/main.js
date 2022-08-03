fe892f50b17cd247581dc461c99e8c22bd79495a
Rafli Athala Jaskandi <rafli.jaskandi@binus.ac.id>
2022-08-03 16:27:26 +0700

    Finishing setup storybook

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,21 +1,17 @@
 const path = require('path')
 
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "staticDirs": ['../public'],
-  "addons": [
+  staticDir: ['../public'],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
     {
-      /**
-       * Fix Storybook issue with PostCSS@8
-       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
-       */
       name: '@storybook/addon-postcss',
       options: {
         postcssLoaderOptions: {
@@ -24,8 +20,8 @@ module.exports = {
       },
     }
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5"
   }
 }
--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -5,7 +5,7 @@ interface ProductProps {
 
 export default function ProductCard({ name, price }: ProductProps) {
     return (
-        <div className="p-10 bg-gray-300 transition-colors hover:bg-gray-400 motion-reduce:transform-none">
+        <div className="p-10 bg-gray-300 transition-colors cursor-pointer hover:bg-gray-400 motion-reduce:transform-none">
             <img src="" alt="" />
             <h1>{ name }</h1>
             <p>{ price.toString() }</p>
